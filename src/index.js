6c19da5852be9024698290c3d73783567e463948
Mathew Dorish <mrd@email.ca>
2018-05-13 15:54:57 -0600

    index.js has been updated to render a single form, with user input and a submit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class UrlForm extends React.Component{
+	render(){
+		return(
+			<div id="parent">
+				<div id="topText">
+					<h1> Enter a URL to have it shortened! </h1>
+				</div>
+
+				<div class="form">
+					<form>
+						<input type="text" id="textField" placeholder="Enter URL" />
+						<input type="submit" value="Submit" id="submitButton"/>
+					</form>
+				</div>
+
+				<div id="bottomOfPage">
+					<p id="bottomText">Made by Mathew Dorish</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+ReactDOM.render(
+	<UrlForm />,
+	document.getElementById('root')
+);
